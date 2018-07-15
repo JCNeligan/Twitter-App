b239899bbcae11ee4eff74c1fd9d7a3e519d5ae6
JackNel <jcneligan@gmail.com>
2018-07-14 22:41:07 -0400

    Get 25 most recent tweets and filter for more than 1 likes

--- index.php
+++ index.php
@@ -8,8 +8,12 @@ $access_token_secret = "nzYtKgvNlnV0J3PPZ1lw2lrFbKLTdQAyMyylZJRfrbUPe";
 $connection = new TwitterOAuth("EzKnztnf40ZPXT7ozJbojPJVd", "Y09dBHgdDdOqduQnk9tuGNc8IDsFx848rooRN7xXpjueEmtg85", $access_token, $access_token_secret);
 $content = $connection->get("account/verify_credentials");
 
-$statues = $connection->post("statuses/update", ["status" => "hello world"]);
-
 $statuses = $connection->get("statuses/home_timeline", ["count" => 25, "exclude_replies" => true]);
 
-print_r($statuses);
+foreach ($statuses as $tweet) {
+    if ($tweet->favorite_count >= 2) {
+        $status = $connection->get("statuses/oembed", ["id" => $tweet->id]);
+        echo $status->html;
+    }
+
+}
